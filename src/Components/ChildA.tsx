5af71511d2c078ad9f9166417062cfcb9aadf252
mdaleardc <ismailaliamot@gmail.com>
2024-11-17 21:10:54 +0600

    passed data to the third component through Props

---
+++ src/Components/ChildA.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const ChildA = ({name}) => {
+  
+  
+  
+  
+  return (
+    <div className='border-[2px] text-white border-[#fff] m-[4px] bg-[#a34] h-[100px]'>
+    <h2 className='text-2xl text-[#00D4FF]'>Hello this is ChildA!</h2>
+    <p>My name is {name}</p>
+    </div>
+    )
+}
+
+
+export default ChildA;
